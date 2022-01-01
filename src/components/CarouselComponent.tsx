e44347dcf31fc673d4846c3100ec033a4eaca792
duidae <duidae@gmail.com>
2022-01-01 17:14:15 +0800

    Add basic components

---
+++ src/components/CarouselComponent.tsx
@@ -0,0 +1,108 @@
+import {useState} from "react";
+import styled from "styled-components";
+import {PhotoData} from "stores";
+
+const Slider = styled.div`
+    position: relative;
+    overflow: visible;
+    width: 100vw;
+`;
+
+const Slide = styled.div`
+    transition-duration: 1s;
+    transition-timing-function: ease-in-out;
+    // transform: scale(1.08);
+`;
+
+const Button = styled.div`
+    background-color: transparent;
+    border: 0;
+    cursor: pointer;
+    height: 43px;
+    opacity: 0.75;
+    outline: none;
+    padding: 0;
+    position: absolute;
+    background-repeat: no-repeat;
+    background-size: contain;
+    margin: 0 1em;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 43px;
+`;
+
+const PrevButton = styled(Button)`
+    left: 0;
+    background-image: url("assets/prev.svg");
+`;
+
+const NextButton = styled(Button)`
+    right: 0;
+    background-image: url("assets/next.svg");
+`;
+
+const Bullets = styled.ol`
+    padding: 0;
+    list-style-type: none;
+    text-align: center;
+    width: 100%;
+    margin: 80px auto 100px auto;
+`;
+
+const Bullet = styled.li`
+    display: inline-block;
+    background-color: #bec0bc;
+    min-width: 10px;
+    min-height: 10px;
+    width: 0.6em;
+    height: 0.6em;
+    margin: 0 0.8em;
+    border-radius: 50%;
+    overflow: hidden;
+    cursor: pointer;
+    text-indent: 100%;
+`;
+
+interface CarouselComponentProps {
+    slides: PhotoData[];
+}
+
+const Carousel = (props: CarouselComponentProps) => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const numSlides = props.slides?.length;
+
+    const onPrevClick = () => {
+        setCurrentSlide(currentSlide === 0 ? numSlides - 1 : currentSlide - 1);
+    };
+
+    const onNextClick = () => {
+        setCurrentSlide(currentSlide === numSlides - 1 ? 0 : currentSlide + 1);
+    };
+
+    const onChangeSlide = (index: number) => {
+        setCurrentSlide(index);
+    };
+
+    if (!Array.isArray(props.slides) || numSlides <= 0) {
+        return null;
+    }
+
+    return (
+        <Slider>
+            <PrevButton title={"上一張"} onClick={onPrevClick} />
+            <NextButton title={"下一張"} onClick={onNextClick} />
+            {props.slides?.map((slide, index) => {
+                return <Slide key={index}>{slide && index === currentSlide && <img src={slide.image} title={slide.desc} alt={slide.desc} className="image" />}</Slide>;
+            })}
+            {numSlides > 0 && (
+                <Bullets>
+                    {props.slides?.map((slide, index) => {
+                        return <Bullet key={`bullet-${index}`} title={`第${index + 1}張`} onClick={() => onChangeSlide(index)} />;
+                    })}
+                </Bullets>
+            )}
+        </Slider>
+    );
+};
+
+export const CarouselComponent = Carousel;
---
+++ src/stores/ImageData.ts
@@ -0,0 +1,31 @@
+export interface PhotoData {
+    image: string;
+    desc: string;
+}
+
+export const PHOTO_DATA: PhotoData[] = [
+    {
+        image: "assets/photo1.jpeg",
+        desc: "民眾仔細看著某人的攝影"
+    },
+    {
+        image: "assets/photo2.jpeg",
+        desc: "漁民無奈的吸了口煙"
+    },
+    {
+        image: "assets/photo3.jpeg",
+        desc: "電廠與漁村"
+    },
+    {
+        image: "assets/photo4.jpeg",
+        desc: "中環抗議現場"
+    },
+    {
+        image: "assets/photo5.jpeg",
+        desc: "Ma Chi Guc Hi"
+    },
+    {
+        image: "assets/photo6.jpeg",
+        desc: "Foodpanda"
+    }
+];
